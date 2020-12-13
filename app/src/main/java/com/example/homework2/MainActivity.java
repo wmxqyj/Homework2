b0b25e46256cca747e8c2ee400eb5e3e0e982bb1
wmxqyj <71748545+wmxqyj@users.noreply.github.com>
2020-12-14 00:39:14 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,35 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
     }
 
+   //登录
     public void Register(View v){
         //判断姓名、密码是否为空
         EditText nameedt = findViewById( R.id.name );
         EditText pwdedt = findViewById( R.id.pwd );
-        ProgressBar probar = findViewById( R.id.pro_bar );
 
         String name = nameedt.getText().toString();
         String pwd = pwdedt.getText().toString();
         if(name.equals( "" ) || pwd.equals( "" )) {
             //为空则提示输入
-
             Toast.makeText(this,"账号或密码不能为空",Toast.LENGTH_SHORT).show();
         }else {
-            //都不为空，则加载进度条
-            probar.setVisibility(View.VISIBLE );
-            new Thread(){
-                @Override
-                public void run() {
-                    for (int i= 0;i <= 100;i++){
-                        probar.setProgress( i );
-                        try {
-                            Thread.sleep( 30 );
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }.start();
-            startActivity( new Intent(this,UserActivity.class) );
+            startActivity( new Intent( MainActivity.this, UserActivity.class ) );
         }
     }
 }
+
